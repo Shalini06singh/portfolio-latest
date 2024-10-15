5e1df6e3bd5393f138a4108d713315be8c5f0c64
Shalini06singh <singh0605shalini@gmail.com>
2024-10-16 00:32:27 +0530

    first commit

---
+++ components/Footer.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+import Link from "next/link";
+import { Button } from "./ui/MovingBorders";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mb-[100px] pb-10 md:mb-5" id="contact">
+      <div className="flex flex-col items-center">
+        <h2 className="heading lg:max-w-[45vw]">
+          Looking forward
+          <span className="text-purple">
+            <div className="p-4 relative z-10 w-full text-center">
+              <Link href={"https://github.com/Shalini06singh"}>
+                <Button
+                  borderRadius="1.75rem"
+                  className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                >
+                  GitHub
+                </Button>
+              </Link>
+              <Link href={"https://www.linkedin.com/in/shalini06/"}>
+                <Button
+                  borderRadius="1.75rem"
+                  className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                >
+                  LinkedIn
+                </Button>
+              </Link>
+            </div>
+          </span>{" "}
+          Contact Me
+        </h2>
+        <p className="text-white-200 md:mt-10 my-5 text-center"></p>
+        <a href="mailto:singh0605shalini@gmail.com">
+          <MagicButton
+            title="Let's get in Touch"
+            icon={<FaLocationArrow />}
+            position="right"
+          />
+        </a>
+      </div>
+
+      <div className="flex mt-16 md:flex-row flex-col justify-center items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright © 2024 Made with ♡ Shalini Singh
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ data/index.ts
@@ -0,0 +1,156 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Skills", link: "#skills" },
+    { name: "Contact", link: "#contact" },
+   {name: "Download Resume",link:" https://drive.google.com/file/d/1loJ8hKEDip200vfIGbhwlSeKN92PP-Gb/view?usp=sharing"}
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize work collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently working on Next.Js Projects",
+      description: "",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Farm2Fork",
+      des: "Comprehensive e-commerce solution built using React Redux Toolkit for dynamic client-side experiences, paired with a Node Express API backend and MongoDB database.",
+      img: "/p1.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/Shalini06singh/Farm2Fork",
+    },
+    {
+      id: 2,
+      title: "Blog Management System",
+      des: "Built using Node.js, React.js, JavaScript, Bootstrap, and REST API.Explore a comprehensive solution for managing blogs, including user authentication and authorization functionalities with a robust login, register, and logout system.",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/Shalini06singh/Blog_Management",
+    },
+    {
+      id: 3,
+      title: "Currency Convertor",
+      des: "API-integrated currency converter built with Vite and React, leveraging custom hooks for efficient state management and functionality.",
+      img: "/p3.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/Shalini06singh/currencyConverter",
+    },
+    {
+      id: 4,
+      title: "Elite Shoppee Front-end Website",
+      des: "Developed and implemented essential e-commerce functionalities from scratch , including search, add-to-cart, checkout and remove-from-cart features with proper authentication.",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/Shalini06singh/E-commerce-App?tab=readme-ov-file",
+    },
+  ];
+  
+
+ 
+
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Technologies",
+      desc: "Assisted in the development of a web-based platform using HTML,CSS,Javascipt,React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Backend Technologies",
+      desc: "Designed and developed using Node.js, Typescript, Express,js.",
+      className: "md:col-span-2", 
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Full Stack Technologies",
+      desc: "Developement and designing using Next.js,Tailwind CSS,Bootsrap, Angular",
+      className: "md:col-span-2", 
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Database Management",
+      desc: "Developed and maintained user-facing features using MongoDB,SQL and Java for problem-solving approach.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link:"https://github.com/Shalini06singh"
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
